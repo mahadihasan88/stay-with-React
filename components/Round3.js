a6936ed8dc9df23b8988f2ae727e58752e2217ad
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2022-11-26 01:15:26 +0600

    Theme context api with functional component

--- components/Round3.js
+++ components/Round3.js
@@ -1,19 +1,37 @@
 
+// import ContextHandler from "./createContext";
+// import FinalRound from "./FinalRound";
+// import React from "react";
+
+// class Round3 extends React.Component{
+
+//     render(){
+//         const {theme, ChangeTheme}=this.context;
+//         return (
+//             <div>
+//                 <FinalRound theme={theme} ChangeTheme={ChangeTheme}/>
+//             </div>
+//         )
+//     }
+// }
+// Round3.contextType=ContextHandler;
+
+// export default Round3;
+
+import { useContext } from "react";
 import ContextHandler from "./createContext";
 import FinalRound from "./FinalRound";
-import React from "react";
 
-class Round3 extends React.Component{
+ function Round3(){
+    const  context = useContext(ContextHandler);
+    const {theme,ChangeTheme}=context;
+    return (
+        <div>
+             <FinalRound theme={theme} ChangeTheme={ChangeTheme}/>
+        </div>
+    )
 
-    render(){
-        const {theme, ChangeTheme}=this.context;
-        return (
-            <div>
-                <FinalRound theme={theme} ChangeTheme={ChangeTheme}/>
-            </div>
-        )
-    }
 }
-Round3.contextType=ContextHandler;
+
 
 export default Round3;
