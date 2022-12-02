db24d1d3564caa349e30494fb5cb92ad907a8b37
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2022-12-02 11:42:40 +0600

    funtionality of useReducer

--- App.js
+++ App.js
@@ -1,37 +1,14 @@
+import React from "react";
+//import Counter from "./components/Counter"
+//import Counter from "./components/ComplexCounter";
+import CounterThree from "./components/CounterThree";
 
-import React, {useCallback, useMemo, useState} from "react";
-import Tittle from "./components/Tittle";
-import Button from "./components/Button";
-import ShowCount from "./components/ShowCount";
 function App(){
-  const [count1,setCount1]=useState(0);
-  const [count2,setCount2]=useState(0);
-  
-  const IncrementByOne=useCallback(()=>{
-    setCount1((prevCount)=>prevCount+1);
-  },[]);
-  const IncrementByFive=useCallback(()=>{
-    setCount2((prevCount)=>prevCount+5);
-  },[]);
-
-  const isEventOrOdd=useMemo(()=>{
-    let i=0;
-    while(i<1000000000){i++;}
-          return (count1%2===0);
-     },[count1]);
-
+ 
   return(
-    <div>
-      
-      <Tittle/>
-          <p>Count Number is : {isEventOrOdd? 'even':'odd'}</p>
-      <ShowCount count={count1} tittle="Counter 1"/>
-      <Button handleClick={IncrementByOne}>IncrementByOne</Button>
-      <hr/>
-      <ShowCount count={count2} tittle="Counter 2"/>
-      <Button handleClick={IncrementByFive}>IncrementByFive</Button>
-    </div>
+    <div className="App">
+       <CounterThree/>
+    </div>  
   )
-
 }
 export default App;
---
+++ components/Counter.js
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+
+let initial=0;
+const reducer=(state,action)=>{
+    switch(action){
+        case 'Increment':
+            return state+1
+        case 'Decrement':
+            return state-1
+
+         default:
+            return state;      
+
+    }
+
+}
+function Counter(){
+    const [count,dispatch]=useReducer(reducer,initial);
+    return(
+        <div>
+            <h2>count-{count}</h2>
+            <button type="button" onClick={()=>dispatch('Increment')}>Increment</button>
+
+            <button type="button" onClick={()=>dispatch('Decrement')}>Decrement</button>
+
+        </div>
+    )
+}
+export default Counter;
