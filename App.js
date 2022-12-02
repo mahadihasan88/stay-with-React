fe71382e317fd47cdd4c2638116b8ba483d39485
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2022-12-02 15:53:29 +0600

    data fetch by using useState ,useReducer and use Effect

--- App.js
+++ App.js
@@ -1,32 +1,13 @@
-import React from "react";
-import { useReducer } from "react";
-import ComponentA from './components/ComponentA';
-export const CounterContext=React.createContext();
-let initial=0;
-const reducer=(state,action)=>{
-    switch(action){
-        case 'Increment':
-            return state+1
-        case 'Decrement':
-            return state-1
-
-         default:
-            return state;      
-
-    }
-
-}
+import GetPost from "./components/GetPost";
+import GetPost2 from "./components/GetPost2";
 
 function App(){
  
- const [count,dispatch]=useReducer(reducer,initial);
+
     return(
         <div className="App">
-            <h2>count-{count}</h2>
-            <CounterContext.Provider value={{counterDispatch:dispatch}}>
-                <ComponentA/>
-            </CounterContext.Provider>
-         
+          {/* <GetPost/> */}
+          <GetPost2/>
         
 
         </div>
